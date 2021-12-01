1e561322111ea353b06bd9cdb54f91f8106c7f66
Johannes Hägglund <johagg17@student.hh.se>
2021-12-01 12:45:30 +0100

    Day1 is done

--- 2021/Day1.py
+++ 2021/Day1.py
@@ -1,17 +1,23 @@
 class Day1():
-    def __init__(self, input):
+    def __init__(self: object, input: list, window: int) -> None:
         self.in_list = input
+        self.window = window
+
+        self.__process()
     
-    def Part1(self):
+    def __process(self: object) -> None: # Private function
+        self.in_list = [int(m.replace('\n', '')) for m in self.in_list]
+
+    def Part1(self: object) -> int:
         '''
         Given a textfile containing values for depth measurment, count the number of times
-         the measurment m increases compared to the previous measurment (m - 1). 
+         the measurment increases compared to the previous measurment. 
         '''
         l = self.in_list
         count = 0
-        previous_m = int(l[0].replace('\n', ''))
+        previous_m = int(l[0])
         for m in l[1:]:
-            new_m = int(m.replace('\n', ''))
+            new_m = int(m)
             if new_m > previous_m:
                 count += 1
             previous_m = new_m
@@ -19,8 +25,22 @@ class Day1():
         self.sol_part1 = count    
         return count
 
-    def Part2(self):
-        pass
+    def Part2(self: object) -> int:
+        '''
+        Same as Part1 with a slightly modification. Instead of looking at each specific measurement, we now 
+        sum togheter 3 mesurments, and then compare it to previous sum. 
+
+        '''
+        wind = self.window
+        list_ = self.in_list
+        prev_sum = sum(list_[:3])
+        count = 0
+        for m in range(1, len(list_)):
+            new_sum = sum(list_[m: m + wind])
+            if new_sum > prev_sum: count += 1
+            prev_sum = new_sum
+
+        return count
 
 
 lines = None
@@ -29,7 +49,8 @@ with open('files\day1.txt') as f:
     f.close()
 
 
-day1 = Day1(lines)
+day1 = Day1(lines, 3)
 solution_part1 = day1.Part1()
-
+solution_part2 = day1.Part2()
 print("Solution Part1: {}".format(solution_part1))
+print("Solution Part2: {}".format(solution_part2))
